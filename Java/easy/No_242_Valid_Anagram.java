48f8dd95cbf284917be9c728e9b6d2771fce82c0
JoeyLiao <joey01271998@gmail.com>
2025-04-12 15:26:53 +0800

    242. Valid Anagram

---
+++ Java/easy/No_242_Valid_Anagram.java
@@ -0,0 +1,36 @@
+// 242. Valid Anagram
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> smap = countingCharacter(s);
+        Map<Character, Integer> tmap = countingCharacter(t);
+
+        if (smap.size() != tmap.size()) {
+            return false;
+        }
+
+        for (Map.Entry<Character, Integer> entry : smap.entrySet()) {
+            if (!tmap.containsKey(entry.getKey()) ||
+                !tmap.get(entry.getKey()).equals(entry.getValue())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private Map<Character, Integer> countingCharacter(String word) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i); 
+            map.put(currentChar, map.getOrDefault(currentChar, 0) + 1);
+        }
+        return map;
+    }
+}
